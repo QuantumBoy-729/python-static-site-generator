0d98c8d18370ec0c60369a4918b877fc12acbbb8
Divyansh Raina <divicoolman729@gmail.com>
2022-10-16 23:22:02 +0530

    Update

---
+++ ssg/content.py
@@ -0,0 +1,46 @@
+from queue import Full
+import re
+from yaml import FullLoader,load
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter,re.MULTILINE)
+
+    def load(self,cls,string):
+        _,fm,content = self.__regex.split(string,2)
+        metadata = load(fm,Loader=FullLoader)
+        return cls(metadata,content)
+    
+    def __init__(self,metadata,content):
+        self.data = metadata
+        self.data["content"]=content
+
+    @property 
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if self.data.has_key("type") else None
+
+    @type.setter
+    def type(self,type):
+        self.data["type"]=type
+    
+    def __getitem__(self,key):
+        return self.data[key]
+    
+    def __iter__(self):
+        return self.data.__iter__()
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        data = {}
+        for key,value in self.data.items():
+            if(key!="content"):
+                data[key]=value
+        
+        return str(data)
